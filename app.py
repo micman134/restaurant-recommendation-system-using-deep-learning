9ad4069376cd7129bedd309098fb0aeb922ef78b
Michael Okoro <michaelokoro1@gmail.com>
2025-06-26 00:23:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ st.markdown(
         left: 0;
         right: 0;
         bottom: 0;
-        background: rgba(0, 0, 0, 0.75);
+        background: rgba(0, 0, 0, 0.85);
         z-index: 0;
     }
     
