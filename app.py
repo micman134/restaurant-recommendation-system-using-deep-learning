9eef1c26ed8116f56bf3b178769127c0e5e24154
Michael Okoro <michaelokoro1@gmail.com>
2025-09-13 07:06:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -211,7 +211,7 @@ if st.session_state.page == "Recommend":
 
             with st.spinner("Searching and analyzing reviews..."):
                 headers = {"accept": "application/json", "Authorization": api_key}
-                params = {"query": food, "near": location, "limit": 20}
+                params = {"query": food, "near": location,"categories": "13065", , "limit": 20}
                 res = requests.get("https://api.foursquare.com/v3/places/search", headers=headers, params=params)
                 restaurants = res.json().get("results", [])
 
